fa68c19fe8b344749f9fe73d85cf2b7affd66907
ayush <ayushrai354@gmail.com>
2026-08-14 19:48:33 +0530

    banner and more responsiveness

--- src/components/common/HomeSecrion.jsx
+++ src/components/common/HomeSecrion.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProductSection from "./ProductSection";
-import Banner from "./Banner";
+import Banner from "./banner";
 
 // =====================================================
 // PRODUCT SECTIONS
